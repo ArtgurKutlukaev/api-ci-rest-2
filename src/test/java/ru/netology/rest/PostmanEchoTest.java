2e2c7f8d875c64a7006ec44becccef03dced7de3
ArturKutlukaev <kutlukayev@bk.ru>
2024-06-09 21:56:41 +0500

    first commit

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -11,7 +11,7 @@ class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("some data")
+                .body("text")
                 .when()
                 .post("/post")
                 .then()
